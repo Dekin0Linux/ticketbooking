f4e11ddbfbd25a7f8ceb9eda8fb676c0e03a96c9
dekin <dekinlinux@gmail.com>
2023-02-02 11:37:18 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin:10,
+        autoplay:true,
+        loop:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        // items:1
+    });
+
+  });
